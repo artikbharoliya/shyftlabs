3e9d735d36579879e3d7cd704e2ed5ab18327cb8
Artik Bharoliya <artikbharoliya67@gmail.com>
2023-02-26 11:24:49 -0800

    feat(Students:API): setup routes for student api

---
+++ server/controller/studentController.js
@@ -0,0 +1,20 @@
+
+exports.createStudent = async (req, res) => {
+  console.log("Post request on student api");
+};
+
+exports.getAllStudents = (req, res) => {
+  console.log("Get request on student api");
+};
+
+exports.getStudent = async (req, res) => {
+  console.log("GET request on student api");
+};
+
+exports.updateStudent = async (req, res) => {
+  console.log("PUT request on student api");
+};
+
+exports.deleteStudent = async (req, res) => {
+  console.log("DEL request on student api");
+};
